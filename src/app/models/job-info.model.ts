d2be5d7bb7ead181dc981e135b1b53cc8a66c63d
Akhil PB <akhil.pb001@gmail.com>
2019-04-27 10:49:49 +0530

    Code formatted

--- src/app/models/job-info.model.ts
+++ src/app/models/job-info.model.ts
@@ -1,37 +1,37 @@
 import * as moment from 'moment';
 
 export class JobInfo {
-  isSelected = false;
-  constructor(
-    public applicationId: string,
-    public usedResource: string,
-    public partition: string,
-    public queueName: string,
-    public submissionTime: number,
-    public allocations: JobAllocation[] | null,
-    public applicationState: string
-  ) {}
+    isSelected = false;
+    constructor(
+        public applicationId: string,
+        public usedResource: string,
+        public partition: string,
+        public queueName: string,
+        public submissionTime: number,
+        public allocations: JobAllocation[] | null,
+        public applicationState: string
+    ) {}
 
-  get formattedSubmissionTime() {
-    const millisecs = Math.round(this.submissionTime / (1000 * 1000));
-    return moment(millisecs).format('YYYY/MM/DD HH:mm:ss');
-  }
+    get formattedSubmissionTime() {
+        const millisecs = Math.round(this.submissionTime / (1000 * 1000));
+        return moment(millisecs).format('YYYY/MM/DD HH:mm:ss');
+    }
 
-  setAllocations(allocs: JobAllocation[]) {
-    this.allocations = allocs;
-  }
+    setAllocations(allocs: JobAllocation[]) {
+        this.allocations = allocs;
+    }
 }
 
 export class JobAllocation {
-  constructor(
-    public allocationKey: string,
-    public allocationTags: string,
-    public uuid: string,
-    public resource: string,
-    public priority: string,
-    public queueName: string,
-    public nodeId: string,
-    public applicationId: string,
-    public partition: string
-  ) {}
+    constructor(
+        public allocationKey: string,
+        public allocationTags: string,
+        public uuid: string,
+        public resource: string,
+        public priority: string,
+        public queueName: string,
+        public nodeId: string,
+        public applicationId: string,
+        public partition: string
+    ) {}
 }
