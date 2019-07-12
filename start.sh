f56ad68c666a7e8d5690d47db4350ef8d3fe89b3
Wilfred Spiegelenburg <wspiegelenburg@cloudera.com>
2019-07-12 18:40:13 +1000

    Changes to README to clarify build process

--- start.sh
+++ start.sh
@@ -1,11 +1,27 @@
 #!/bin/sh
 
+#
+# Copyright 2019 Cloudera, Inc.  All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
 yarn install
 
 rm -rf ./dist
 
 yarn build:prod
 
-docker build -t yunikorn/scheduler-web:0.1.0 -f ./nginx/Dockerfile .
+docker build -t yunikorn/yunikorn-web:0.1.0 -f ./nginx/Dockerfile .
 
-docker run -d -p 8089:80 yunikorn/scheduler-web:0.1.0
+docker run -d -p 9889:80 yunikorn/yunikorn-web:0.1.0
