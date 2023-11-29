9d559871125b14218f3fdbcc82225be2d0560e1a
wusamzong <t3742238@gmail.com>
2023-11-29 10:38:24 +0800

    deal with doupache's suggestion

--- src/app/components/nodes-view/highlighttable-search.pipe.ts
+++ src/app/components/nodes-view/highlighttable-search.pipe.ts
@@ -6,7 +6,11 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class HighlightSearchPipe implements PipeTransform {
 
   transform(value: string, search: string): string {
-    const valueStr = value + ''; // Ensure numeric values are converted to strings
+    const valueStr = String(value); // Ensure numeric values are converted to strings
+    // (?![^&;]+;) - to ensure that there are no HTML entities (such as &amp; or &lt;) 
+    //               ex. value = "&lt;span&gt;" search = "span", The word 'span' will not be included in the matches
+    // (?!<[^<>]*) - to ensure that there are no HTML tags (<...>)
+    //               ex. value = "<span>" search = "span", The word 'span' will not be included in the matches
     return valueStr.replace(new RegExp('(?![^&;]+;)(?!<[^<>]*)(' + search + ')(?![^<>]*>)(?![^&;]+;)', 'gi'), '<strong>$1</strong>');
   }
 
