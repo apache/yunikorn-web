478d7c1ff1cd028718f8cd37e5660568ce4926a4
Michael Chu <michaelchu@MichaeldeMacBook-Pro.local>
2025-06-05 18:02:02 +0800

    fix karma require path

--- karma.conf.ci.js
+++ karma.conf.ci.js
@@ -24,14 +24,14 @@ process.env.CHROME_BIN = require('puppeteer').executablePath();
 module.exports = function (config) {
   config.set({
     basePath: '',
-    frameworks: ['jasmine', '@angular-devkit/build-angular'],
+    frameworks: ['jasmine', '@angular/build'],
     plugins: [
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage'),
       require('karma-coverage-istanbul-reporter'),
-      require('@angular-devkit/build-angular/plugins/karma'),
+      require('@angular/build/plugins/karma'),
       require('karma-super-dots-reporter'),
     ],
     client: {
